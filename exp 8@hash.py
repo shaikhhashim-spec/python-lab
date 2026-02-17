9dc97353b3a56dfd32fda9e05e01265cbd991de8
shaikhhashim-spec <shaikhhashim@eng.rizvi.edu.in>
2026-02-17 09:46:17 +0530

    Add files via upload

---
+++ exp 8@hash.py
@@ -0,0 +1,11 @@
+#https://github.com/saadkhan17406-sketch/Python
+print("UIN : 251A053", "DATE : 09-02-2026")
+s = int(input("Enter size of square: "))
+
+for i in range(s):
+    print("* "*s)
+
+rows = int(input("Enter number of rows: "))
+
+for i in range(1, rows+1):
+    print("* "*i)
