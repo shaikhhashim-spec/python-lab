36068a85a2632f8ee81ed8d51bc528f0b60551f5
shaikhhashim-spec <shaikhhashim@eng.rizvi.edu.in>
2026-02-17 09:51:43 +0530

    Add files via upload

---
+++ exp 9@hash.py
@@ -0,0 +1,8 @@
+#https://github.com/shaikhhashim-spec/Python
+#FACTORIAL GENERATOR
+print("UIN : 251A053  DATE : 10-02-2026")
+n = int(input("ENTER NUMBER : "))
+fact = 1
+for i in range(1, n+1):
+  fact*=i
+print("FACTORIAL = ", fact)
