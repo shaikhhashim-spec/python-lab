5f7a05a850fbadbce57031e0cccdcd723122dd3b
shaikhhashim-spec <shaikhhashim@eng.rizvi.edu.in>
2026-03-26 19:37:47 +0530

    Create Exp15@hash.py

---
+++ Exp15@hash.py
@@ -0,0 +1,11 @@
+print("Hashim, 251A011")
+
+def divide(a, b):
+    result = a / b   # Error if b = 0
+    return result
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+output = divide(num1, num2)
+print("Result:", output)
